99f931992803a29bb09e5749d18d14a29b094688
Durga0808 <bejawada.durgaprasad@gmail.com>
2024-05-28 22:59:53 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './../styles/App.css';
+import WeatherDisplay from './WeatherDisplay';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: 'Unknown' });
+
+  useEffect(() => {
+    // Simulate fetching weather data
+    const weatherInput = { temperature: 25, conditions: 'Sunny' };
+    setWeather(weatherInput);
+  }, []);
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <header className="App-header">
+        <WeatherDisplay temperature={weather.temperature} conditions={weather.conditions} />
+      </header>
+
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,18 @@
+// src/WeatherDisplay.js
+import React from 'react';
+
+const WeatherDisplay = ({ temperature, conditions }) => {
+  const temperatureStyle = {
+    color: temperature > 20 ? 'red' : 'blue'
+  };
+
+  return (
+    <div>
+      <h1>Weather Conditions</h1>
+      <p style={temperatureStyle}>Temperature: {temperature}°C</p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
